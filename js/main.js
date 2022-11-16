7f3d0589965fe9336046db75d6871fa3f669fb54
Alec Dergachev <alecwithac27@gmail.com>
2022-11-16 16:57:56 -0500

    linking the data sheet

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function songs(json) {
+    let message = document.getElementsByClassName(`carousel-slides`);
+
+    message.textContent = json[0].artist;
+    console.log (json[0].artist);
+}
+
+window.onload = () => {
+    let body = document.querySelector(`body`);
+    let script = document.createElement(`script`);
+    script.setAttribute(`src`,`json/data.json`);
+    body.append(script);
+}
